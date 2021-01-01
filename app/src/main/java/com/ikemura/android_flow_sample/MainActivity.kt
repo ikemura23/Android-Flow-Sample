d17040726d076ad69e30322c233b2e846e5185da
ikemura23 <banbara.work@gmail.com>
2021-01-01 16:08:19 +0900

    flow 実装

--- app/src/main/java/com/ikemura/android_flow_sample/MainActivity.kt
+++ app/src/main/java/com/ikemura/android_flow_sample/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.ikemura.android_flow_sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startFlow()
+    }
+
+    private fun startFlow() {
+        GlobalScope.launch {
+            val myFlow = flow<String> {
+                delay(1000)
+                emit("hello flow")
+            }
+
+            myFlow.collect { println(it) }
+        }
     }
 }
